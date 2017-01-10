acd8c47dff8517e512b799bc6abf5343174c4607
Wade Foster <jwade109@vt.edu>
2017-01-09 20:42:33 -0500

    Added all the code from last year

---
+++ SOFHIoaewf/SOFHIoaewf.ino
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void setup()
+{
+  
+  
+  char fool = "57";
+  
+  unsigned int april = atoi(fool);
+  
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  
+}
