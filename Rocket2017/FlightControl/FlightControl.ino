451b1f75e46b8740ebcf9fb0c9f7a8c00628c4f1
Wade Foster <jwade109@vt.edu>
2017-04-14 02:47:14 -0400

    Added kalman implementation, stage progression logic

--- Rocket2017/FlightControl/FlightControl.ino
+++ Rocket2017/FlightControl/FlightControl.ino
@@ -10,8 +10,7 @@
 
 #define NOMINAL_DT                  0.05 // seconds
 #define INDICATOR_PIN               3 // blinks with every tick
-#define STAGE_PIN                   4 // blinks according to stage
-#define ERROR_PIN                   5 // turns on if a fatal error occurs
+#define ERROR_PIN                   4 // turns on if a fatal error occurs
 
 #define ON_LAUNCHPAD                0 // indicates rocket is on the launchpad. accel = 0.
 #define MOTOR_BURN                  1 // indicates motor is burning. accel > 0.
@@ -22,21 +21,21 @@ int stage = ON_LAUNCHPAD;           // current state of the vehicle
 Adafruit_LSM303_Accel_Unified       lsm = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_BMP085_Unified             bmp = Adafruit_BMP085_Unified(10085);
 
-KalmanFilter filter(0.5, 0.01);
+KalmanFilter filter(0,0.0001);
 File sensorData;
-
-void update_time(double* current_time, double* dt);
-void update_indicator();
-void fatal_error(int error);
 
 void setup()
 {
-    Serial.begin(9600);
-
     // set the pinmode for the diagnostic LEDs
     pinMode(INDICATOR_PIN, OUTPUT);
-    pinMode(STAGE_PIN, OUTPUT);
     pinMode(ERROR_PIN, OUTPUT);
+
+    // flash the LEDs to ensure they're all working
+    digitalWrite(INDICATOR_PIN, HIGH);
+    digitalWrite(ERROR_PIN, HIGH);
+    delay(1000);
+    digitalWrite(INDICATOR_PIN, LOW);
+    digitalWrite(ERROR_PIN, LOW);
 
     // initialize the LSM303, the BMP085, and the SD card
     if(!lsm.begin() || !bmp.begin() || !SD.begin())
@@ -45,11 +44,12 @@ void setup()
     }
 
     // initialize the log file
-    char filename[] = "LOG00.csv";
-    for (uint8_t i = 0; i < 100; i++)
+    char filename[] = "LOG000.csv";
+    for (uint8_t i = 0; i < 1000; i++)
     {
-        filename[3] = i/10 + '0';
-        filename[4] = i%10 + '0';
+        filename[3] = i/100 + '0';
+        filename[4] = (i/10)%10 + '0';
+        filename[5] = i%10 + '0';
 
         if (!SD.exists(filename))
         {
@@ -62,47 +62,69 @@ void setup()
         fatal_error(2);
     }
 
+    // initialize the Kalman filter
+    // state transition matrix, F
+    filter.F[0][0] = 1;
+    filter.F[1][1] = 1;
+    // sensor covariance matrix, R
+    filter.R[0][0] = 0.08;
+    filter.R[1][1] = 0.02;
+    // state matrix, X
+    filter.X[0][0] = 0;
+    filter.X[1][0] = 0;
+    // uncertainty matrix, P
+    filter.P[0][0] = 100;
+    filter.P[1][1] = 100;
+
     sensorData.println(filename);
-    sensorData.println("time,alt,accel");
+    sensorData.println("time,alt,accel,vel");
     delay(NOMINAL_DT * 1000);
 }
 
 void loop()
 {
     update_indicator();
+    static double alt_prev;
+    double current_time, dt, altitude, accel;
+    update_values(&current_time, &dt, &altitude, &accel);
 
-    double current_time, dt;
-    update_time(&current_time, &dt);
+    // filter wizardry to clean up alt and accel data
+    filter.F[0][1] = 0.2 * dt;
+    float Z[2] = {altitude, accel};
+    float* X = filter.step((float*) Z);
+    altitude = X[0];
+    accel = X[1];
+    double velocity = (altitude - alt_prev)/dt;
 
-    sensors_event_t alt_event;        // altitude event from BMP085
-    sensors_event_t accel_event;      // acceleration event from LSM303
-
-    float altitude = 0;
-    float accel = 0;
-
-    bmp.getEvent(&alt_event);
-    altitude = bmp.pressureToAltitude(SENSORS_PRESSURE_SEALEVELHPA, alt_event.pressure);
-
-    lsm.getEvent(&accel_event);
-    accel = accel_event.acceleration.x; // acceleration along longitudinal axis
-
-    if (FLIGHT_NUMBER) // this block only runs during the second flight
+    // stage-specific progression logic
+    if (stage == ON_LAUNCHPAD && velocity > 20)
     {
-        int vel = 100;
-        double ta = Equation::t_a(vel, DRY_MASS, K_ACTIVE);
-        double alt = Equation::alt(alt, vel, DRY_MASS, K_ACTIVE, ta);
-
-        float Z[MEAS] = {alt, accel};
-        float* X = filter.step((float*) Z);
+        stage++;
+    }
+    if (stage == MOTOR_BURN)
+    {
+        static int low_accel_count;
+        if (accel < -10) low_accel_count++;
+        if (low_accel_count > 15) stage++;
+    }
+    if (stage == APOGEE_COAST && FLIGHT_NUMBER)
+    {
+        // motor control routine
     }
 
+    alt_prev = altitude; // save previous altitude for faux derivative
+
+    // write to log file
     sensorData.print(current_time, 4);
     sensorData.print(",");
     sensorData.print(altitude);
     sensorData.print(",");
-    sensorData.println(accel);
+    sensorData.print(accel);
+    sensorData.print(",");
+    sensorData.println(velocity);
     sensorData.flush();
 
+    // wait for next tick if computations were fast enough
     double compTime = (double) millis()/1000 - current_time;
     if (compTime < NOMINAL_DT)
     {
@@ -110,12 +132,22 @@ void loop()
     }
 }
 
-void update_time(double* current_time, double* dt)
+void update_values(double* current_time, double* dt, double* alt, double* accel)
 {
+    // get current time and time since last call
     static double lastTime;
     *current_time = (double) millis()/1000;
     *dt = *current_time - lastTime;
     lastTime = *current_time;
+
+    // collect acceleration and altitude measurements
+    sensors_event_t alt_event;        // altitude event from BMP085
+    sensors_event_t accel_event;      // acceleration event from LSM303
+    bmp.getEvent(&alt_event);
+    *alt = bmp.pressureToAltitude(SENSORS_PRESSURE_SEALEVELHPA,
+        alt_event.pressure) - ALT_BIAS;
+    lsm.getEvent(&accel_event);
+    *accel = accel_event.acceleration.z - ACCEL_BIAS;
 }
 
 void update_indicator()
@@ -123,7 +155,7 @@ void update_indicator()
     static bool indicator;
     if (indicator)
     {
-        analogWrite(INDICATOR_PIN, 20);
+        analogWrite(INDICATOR_PIN, 50);
     }
     else
     {
@@ -134,6 +166,6 @@ void update_indicator()
 
 void fatal_error(int error)
 {
-    analogWrite(ERROR_PIN, 255);
+    digitalWrite(ERROR_PIN, HIGH);
     while(1);
 }
